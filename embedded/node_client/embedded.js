299664d32a4f4e0ed50ae56ffa377f170d93157d
Jose Apari-Pantigozo <jap@localhost.localdomain>
2020-01-25 22:15:00 -0500

    Add node-gif as dep + test creating gif

--- embedded/node_client/embedded.js
+++ embedded/node_client/embedded.js
@@ -5,7 +5,10 @@ import {PythonShell} from 'python-shell';
 import io from 'socket.io-client';
 import debug from 'debug';
 import crypto from 'asymmetric-crypto';
+import fs from 'fs';
 import keys from './2keys.js';
+import {Buffer} from 'buffer';
+import GifLib from 'node-gif';
 
 const socket = io('http://18.188.99.138:8080/');
 const dbg = debug('embedded');
@@ -54,14 +57,31 @@ socket.on('open', () => {
     });
     
     takePic();
-    takePicsTask = setInterval(takePic, 10000);
+    takePicsTask = setInterval(takePic, 8000);
     
     // Test Code
-    dbg("Stopping in 44 seconds");
+    dbg("Stopping in 20 seconds");
     let stop = setTimeout(() => {
         clearInterval(takePicsTask);
-        dbg(picsTaken, "pictures taken.")
-    }, 44000);
+        dbg(picsTaken, "pictures taken.");
+        var chunkDirs = fs.readdirSync('.').sort().filter(
+            function (f) {
+                return /^image_/.test(f)
+            }
+        );
+        
+        dbg("Generating GIF...");
+        var animatedGif = new GifLib.AnimatedGif(1280,720);
+        
+        chunkDirs.forEach(function (file) {
+            dbg(file);
+            var rgb = fs.readFileSync(file); // returns buffer
+            animatedGif.push(rgb, 0, 0, 1280, 720);
+        });
+        animatedGif.endPush();
+        var gif = animatedGif.getGif();
+        fs.writeFileSync('animated.gif', gif.toString('binary'), 'binary');
+    }, 20000);
 });
 
 socket.on('getOTT', (msg) => {
