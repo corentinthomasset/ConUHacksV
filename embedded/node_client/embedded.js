0a8f3e317ebb6f22119e7c56f0936160d4e121a7
a@a.ca <a@a.ca>
2020-01-26 08:10:26 -0500

    Last?

--- embedded/node_client/embedded.js
+++ embedded/node_client/embedded.js
@@ -10,14 +10,24 @@ import keys from './2keys.js';
 import im from 'imagemagick';
 import request from 'request';
 
-const MAIN_URL = 'http://172.30.191.200';
+//const MAIN_URL = 'http://172.30.191.200';
+const MAIN_URL = 'http://18.188.99.138';
 
 const socket = io(MAIN_URL+':8080/');
+
 const dbg = debug('embedded');
 const dbgOpen = debug('embedded:open');
 
 let takePicsTask = null;
+let cameraTaken = false;
 let picsTaken = 0;
+
+//let modec = new PythonShell('../hw_control/motion-2.py')
+//modec.on('message', (msg) => {
+//    if (msg == "Mo!") {
+//	takePic();
+//    }
+//});
 
 function leds(arg) {
     let options = {
@@ -53,17 +63,20 @@ function single_buzz() {
     });
 }
 
-const takePic = () => {
+function takePic() {
+    if (cameraTaken) return;
+    cameraTaken = true;
     let cameraOptions = {
         args: [picsTaken+1]
     };
     dbgOpen("Taking picture", picsTaken+1, " in 3... 2... 1... SNAP");
     var cameraShell = new PythonShell('../hw_control/camera.py', cameraOptions);
     cameraShell.end(function (err) {
-        picsTaken = picsTaken + 1;
         if (err) {
             throw err;
         };
+        picsTaken = picsTaken + 1;
+        cameraTaken = false
     });
 };
 
@@ -71,12 +84,12 @@ dbg("----");
 
 socket.on('connect', () => {
     dbg("Connection with C&C Server established.");
-    socket.emit('box_id', keys.publicKey)
+    socket.emit('box_id', keys.publicKey, "corentin@me.io");
     dbg("Pub Key Sent: ", keys.publicKey);
 });
 
-socket.on('open', () => {
-    if (takePicsTask !== null) {
+socket.on('unlock', () => {
+    if (takePicsTask !== null || cameraTaken) {
         dbgOpen("Open event while already in Open sequence ??")
         return;
     }
@@ -84,6 +97,7 @@ socket.on('open', () => {
 
     door(0);
     leds(1);
+    single_buzz();
 
     // Clean images before starting a new process.
     picsTaken = 0;
@@ -105,14 +119,14 @@ socket.on('singleBuzz', () => {
     single_buzz();
 });
 
-socket.on('validateDelivery', () => {
+socket.on('lock', () => {
     dbgOpen("Validating Delivery");
     clearInterval(takePicsTask);
     takePicsTask = null;
     
     door(1);
     single_buzz();
-    sleep.sleep(2); // Because sometimes a picture cant be written to disk fast enough before the convert happens? MAYBE
+    sleep.sleep(2); //Because lights stay on if too quick after door(1) + Because sometimes a picture cant be written to disk fast enough before the convert happens? MAYBE
     leds(0);
 
     dbgOpen(picsTaken, "pictures taken.");
@@ -120,17 +134,17 @@ socket.on('validateDelivery', () => {
             function(err, stdout){
                 if (err) {dbg(err);process.exit();}
 		dbgOpen("Gif Generated.");
-    		sleep.sleep(3);
+    		sleep.sleep(2);
 		dbgOpen("Sending gif...");
  		const formData = {
 			spycam: fs.createReadStream('res.gif'),
 		};
-    
     		request.post({url:MAIN_URL+':9999/upload/'+encodeURIComponent(keys.publicKey), formData: formData}, (err, res, body) => {
     			if (err) {
         			dbgOpen("Error on upload", err);
 		    	} else {
 			    	dbgOpen('Gif uploaded');
+				socket.emit("lock");
 			}
     		});
 	    }
